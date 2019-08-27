53c5131558fbca162d59f21b959335cf4dc798cd
Adam Stokes <battlemidget@users.noreply.github.com>
2019-08-27 16:06:34 -0400

    cleanup
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

---
+++ tasks.py
@@ -0,0 +1,38 @@
+from invoke import task
+
+
+@task
+def clean(c):
+    c.run("rm -rf build dist ogc.egg-info __pycache__")
+
+
+@task
+def fix(c):
+    c.run("isort -rc -m 3 .")
+    c.run("black .")
+
+
+@task
+def test(c):
+    c.run("pylint ogc_plugins_spec_doc.py")
+    c.run("pytest")
+
+
+@task
+def bump_rev(c):
+    c.run("punch --part patch")
+
+
+@task
+def dist(c):
+    c.run("python3 setup.py bdist_wheel")
+
+
+@task
+def install(c):
+    c.run("pip install --upgrade dist/*whl --force")
+
+
+@task
+def upload(c):
+    c.run("twine upload dist/*")
